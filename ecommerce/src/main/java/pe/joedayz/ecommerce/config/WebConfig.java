f348f832d041ee2669f6162d837489fadf5222da
Gianmarcos <devantryx@email.com>
2026-07-07 11:38:22 -0500

    test git integratrion

--- ecommerce/src/main/java/pe/joedayz/ecommerce/config/WebConfig.java
+++ ecommerce/src/main/java/pe/joedayz/ecommerce/config/WebConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig
 {
+    //nuevo cambio.
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
